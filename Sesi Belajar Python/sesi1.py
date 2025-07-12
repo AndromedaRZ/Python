dc4d155b7edca1865c6ecd51153658940bedde07
Rizky <rizkyricky859@gmail.com>
2025-07-12 12:44:04 +0700

    update folder sesi belajar

--- Sesi Belajar Python/sesi1.py
+++ Sesi Belajar Python/sesi1.py
@@ -1,5 +1,6 @@
 # Belajar python sesi 1
 import random
+import sys
 
 welcome_message = "Selamat datang di CUYPY GAMES"
 posisi_cuypy = random.randint(1, 4)
@@ -21,20 +22,16 @@ print(f'''Halo! {nama_anda}, coba perhatikan goa di bawah ini
       |_|  |_|  |_|  |_|
       ''')
 
-while True:
-    print("Menurut kamu, di goa nomor berapakah CUYPY berada? [1 / 2 / 3 / 4 ]")
-    jawaban = int(input("Masukkan jawabanmu: "))
-    print()
-    konfirmasi = input(f"Apakah kamu yakin bahwa CUYPY ada di goa nomor {jawaban} [y/n]")
-    print()
-    if konfirmasi == "y":
-        break
-    elif konfirmasi == "n":
-        print("Tidak yakin!, coba lagi")
-    else:
-        print("Silahkan masukkan jawaban yang benar! y/n")
+print("Menurut kamu, di goa nomor berapakah CUYPY berada? [1 / 2 / 3 / 4 ]")
+jawaban = int(input("Masukkan jawabanmu: "))
 
-if jawaban == posisi_cuypy:
-    print(f"Selamat {nama_anda}, kamu menemukan CUYPY di goa nomor {posisi_cuypy} dan pilihanmu adalah goa nomor {jawaban}")
+konfirmasi = input(f"Apakah kamu yakin bahwa CUYPY ada di goa nomor {jawaban}? [y/n]:")
+
+if konfirmasi == "n":
+    print("Silahkan mulai ulang programnya")
+    sys.exit()
 else:
-    print(f"Maaf {nama_anda}, CUYPY tidak berada di goa yang kamu pilih, tapi ada di goa nomor {posisi_cuypy}, sementara kamu memilih goa nomor {jawaban}")
+    if jawaban == posisi_cuypy:
+        print(f"Selamat {nama_anda}, kamu menemukan CUYPY di goa nomor {posisi_cuypy} dan pilihanmu adalah goa nomor {jawaban}")
+    else:
+        print(f"Maaf {nama_anda}, CUYPY tidak berada di goa yang kamu pilih, tapi ada di goa nomor {posisi_cuypy}, sementara kamu memilih goa nomor {jawaban}")
