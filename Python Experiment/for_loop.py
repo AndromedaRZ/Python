be411591768b0bc781fdc7ef91869a8c15de3701
Rizky <rizkyricky859@gmail.com>
2025-07-16 20:22:48 +0700

    menambah file eksperimen

---
+++ Python Experiment/for_loop.py
@@ -0,0 +1,14 @@
+# import random
+
+# # Minta jumlah lemparan dari user
+# jumlah_lempar = int(input("Mau lempar dadu berapa kali? "))
+
+# # Loop sebanyak jumlah lemparan
+# for i in range(jumlah_lempar):
+#     hasil = random.randint(1, 6)
+#     print(f"Lemparan ke-{i+1}: 🎲 {hasil}")
+
+total = 0
+for n in range(1, 101):
+    total += n
+print(f"Total jumlah dari 1 sampai 100 adalah: {total}")
