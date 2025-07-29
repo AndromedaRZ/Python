26e14cc0d957c65040af6706fa7d97579f7748cf
Rizky <rizkyricky859@gmail.com>
2025-07-29 16:06:33 +0700

    Menambah file belajar

---
+++ Belajar Python/Belajar_10_Operator_assignment/main.py
@@ -0,0 +1,36 @@
+# Operator Assignment: Operasi yang dapat dilakukan dengan penyingkatan
+# Operasi ditambah dengan assignment
+
+a = 10 # adalah assignment
+print(f"nilai a = {a}")
+
+# contoh penyingkatan yang umum digunakan yaitu pada operasi artimatika
+
+# operasi penjumlahan 
+a += 5 # artinya adalah a = a + 5
+print(f"nilai a += 5, nilai a menjadi{a}")
+
+# operasi pengurangan
+a -= 5 # artinya adalah a = a - 5
+print(f"nilai a -= 5, nilai a menjadi {a}")
+
+# operasi perkalian
+a *= 5 # artinya adalah a = a * 5
+print(f"nilai a *= 5, nilai a menjadi {a}")
+
+# operasi pembagian
+a /= 5 # artinya adalah a = a / 5
+print(f"nilai a /= 5, nilai a menjadi {a}")
+
+b = 20
+print(f"nilai b = {b}")
+
+# operasi modulus
+b %= 3 # artinya adalah b = b % 3
+print(f"nilai b %= 3, nilai b menjadi {b}")
+
+# operasi floor division
+b = 10
+print(f"nilai b = {b}")
+b //= 3 # artinya adalah b = b // 3
+print(f"nilai b //= 3, nilai b menjadi {b}")
