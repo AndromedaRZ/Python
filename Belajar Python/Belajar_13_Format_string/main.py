a8fa0c1a7222b3106f312dc69ed228360788bbf1
Rizky <rizkyricky859@gmail.com>
2025-08-01 09:24:33 +0700

    update file belajar

--- Belajar Python/Belajar_13_Format_string/main.py
+++ Belajar Python/Belajar_13_Format_string/main.py
@@ -32,6 +32,52 @@ format_str = f"ribuan = {angka:,}" # fungsi ':,' adalah untuk memasukkan koma se
 print(format_str)
 
 # bilangan desimal
-angka = 2005.5
-format_str = f"desimal = {angka}"
+angka = 2005.54321
+format_str = f"desimal = {angka:.2f}" # tanda titik '.' menandakan desimalnya, lalu fungsi '2f' adalah mengambil sejauh mana angka floatnya, dalam kasus ini karena angkanya '2' maka jumlah angka float (yang dibelakang koma) yang diambil hanya 2 digit 
 print(format_str)
+
+# menampilkan leading zero
+angka = 2005.54321
+format_str = f"desimal = {angka:9.3f}" # angka '9' berfungsi untuk menambahkan sesuatu didalam variabel 'angka'
+print(format_str)
+
+# contoh lain leading zero
+angka = 2005.54321
+format_str = f"desimal = {angka:09.3f}" # angka '9' berfungsi untuk menambahkan sesuatu didalam variabel 'angka', dan yang ditambahkan adalah angka '0' disebelah angka '9' tersebut
+print(format_str)
+
+# menampilkan tanda + atau -
+angka_minus = -10
+angka_plus = 10
+format_minus = f"minus = {angka_minus}" # jika diprint, simbol mines (-) akan terlihat
+format_plus = f"plus = {angka_plus}" # jika diprint, simbol plus (+) tidak akan terlihat
+format_minus2 = f"minus = {angka_minus:+d}" # jika diprint, simbol mines (-) akan tetap terlihat
+format_plus2 = f"plus = {angka_plus:+d}" # jika diprint, simbol plus (+) akan terlihat
+
+print(format_minus)
+print(format_plus)
+print(format_minus2)
+print(format_plus2)
+
+# memformat persen
+persentase = 0.045
+format_persen = f"persen = {persentase:.2%}" # fungsi dari ':' adalah untuk memulai bagian format, diikuti dengan '.2' yang berarti dua angka di belakang koma, dan simbol persen (%) untuk mengubah angka desimal menjadi persentase (dikalikan 100 dan ditambah dengan tanda %)
+print(format_persen) 
+
+# melakukan operasi aritmatika di dalam placeholder
+harga = 10000
+jumlah = 5
+
+format_string = f"harga total = {harga*jumlah:,}" # kita bisa melakukan operasi aritmatika didalam kurung kurawal yang berisi variabel harga dan jumlah
+print(format_string)
+
+# format angka lain (binary, octal, hexadecimal)
+
+angka = 255
+format_binary = f"binary = {bin(angka)}" # konversi nilai dari variabel angka ke bilangan biner
+format_octal = f"octal = {oct(angka)}" # konversi nilai dari variabel angka ke bilangan octal
+format_hex = f"hex = {hex(angka)}" # konversi nilai dari variabel angka ke bilang hexadesimal
+
+print(format_binary)
+print(format_octal)
+print(format_hex)
