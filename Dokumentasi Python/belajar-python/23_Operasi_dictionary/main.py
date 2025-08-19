56d55eae4dd2345c374bd41362dcfb0cdf6f5145
Ricky <rizkykricky2209@gmail.com>
2025-08-19 10:24:16 +0700

    apa sajalah

---
+++ Dokumentasi Python/belajar-python/23_Operasi_dictionary/main.py
@@ -0,0 +1,43 @@
+# operator dictionary
+
+data_dict = {
+    'cup':'ucup surucup',
+    'tong':'otong surotong',
+    'dung':'dudung surudung'
+}
+
+# panjang dictionary
+LENDICT = len(data_dict) # LENDICT dibuat uppercase karena itu adalah constant
+
+print(f"Panjang dictionary: {LENDICT}")
+
+# mengecek suatu 'key' apakah ada (exist) atau tidak
+
+KEY = 'cup' # pertama-tama masukkan key ke dalam constant
+CHECKKEY = KEY in data_dict # lalu buat fungsi untuk mengecek apakah key tersebut ada di dalam dictionary data_dict?
+print(f"Apakah {KEY} ada di data_dict: {CHECKKEY}") # bisa kita lihat dengan menjalankan perintah ini, hasilnya akan menunjukkan True atau False
+
+# mengakses value (read) dengan get
+print(data_dict['cup'])
+print(data_dict.get('cup')) # hasilnya akan sama seperti baris di atas karena data tersebut ada di dalam data_dict, bagaimana jika datanya tidak ada?
+# contohnya di bawah ini
+print(data_dict.get('kis')) # hasilnya akan menjadi 'none' ketika data yang kita cari tidak ada di dalam dictionary, jika kita menuliskan data yang tidak ada tanpa menggunakan fungsi .get() maka programnya akan langsung error
+# fungsi lain dari .get() ini kita bisa menambahkan kata untuk ditampilkan ketika data yang diinginkan tidak ada di dalam dictionary
+print(data_dict.get('kis','key tidak ditemukan')) # mengecek key dengan message tidak ditemukan
+
+
+# mengupdate data (mengubahh data)
+data_dict['cup'] = "ucup si ganteng"
+print(data_dict)
+
+data_dict['sep'] = "asep si kecee" # bisa dianggap menambahkan data
+print(data_dict)
+
+data_dict.update({'cup':'ucup surucup'}) # ketika kita menggunakan fungsi .update(), jika data sebelumnya telah ada, maka ia akan merubah atau mengupdate datanya sesuai perubahan terbaru
+print(data_dict)
+data_dict.update({'faqih':'faqihza si keren'}) # tetapi ketika sebelumnya datanya tidak ada di dalam dictionarynya, maka otomatis data yang kita tulis akan masuk ke dalam dictionary tersebut
+print(data_dict)
+
+# menghapus data pada dictionary
+del data_dict['faqih'] # kita hanya perlu mengetik del lalu masukkan nama dictionary dan key yang ingin dihapus
+print(data_dict) # maka hasilnya key tersebut akan hilang
