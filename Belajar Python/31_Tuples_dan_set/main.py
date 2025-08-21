dbbcad1713b255d8fbeaa5978b9d625c1be5e413
Rizky <rizkyricky859@gmail.com>
2025-08-21 15:21:53 +0700

    update file belajar

--- Belajar Python/31_Tuples_dan_set/main.py
+++ Belajar Python/31_Tuples_dan_set/main.py
@@ -10,10 +10,10 @@ data_tuples = (1, 2, 3, 4) # menggunakan kurung biasa
 print(data_list)
 print(data_tuples)
 
-# perbedaan data list biasa dengan data tuples adalah data tuples tidak bisa diubah nilainya
+# perbedaan data list biasa dengan data tuples adalah data tuples tidak bisa diubah isinya (immutable)
 
 data_list[0] = 5
-# data_tuples[0] = 5 # Jika kode ini diaktifkan maka akan menimbulkan error karena data_tupples tidak bisa diassignment
+# data_tuples[0] = 5 # Jika kode ini diaktifkan maka akan menimbulkan error karena data_tupples tidak bisa diubah isinya
 
 # sesudah diubah nilainya
 print(data_list)
@@ -39,3 +39,8 @@ print(data_sets)
 
 # karena data sets tidak memiliki index, kita tidak bisa mengambil index dari data sets
 # print(data_sets[0]) # akan error jika diaktifkan
+
+data_sets = {"ucup", "ucup", "ani", "ani", "dedi", "dedi"} # urutan data dalam sets juga tidak pasti
+data_sets2 = {5, 5, 5, 2, 2, 2, 3, 3, 3, 4, 4, 4} # dalam data sets, tidak boleh ada nilai atau elemen yang sama dalam satu data sets tersebut. Jika ada yang sama, maka hanya akan mengambil satu nilai atau elemen yang sama tersebut
+print(data_sets)
+print(data_sets2)
---
+++ Belajar Python/33_Operasi_dictionary/main.py
@@ -0,0 +1,44 @@
+# Operator Dictionary
+
+data_dict = {
+    "cup":"ucup surucup",
+    "tong":"otong surotong",
+    "dung":"dung surudung",
+}
+
+print(data_dict)
+
+# panjang dictionary
+LENDICT = len(data_dict)
+print(f'panjang dictionary: {LENDICT}')
+
+# mengecek key nya ada atau tidak
+KEY = "cup"
+CHECKKEY = KEY in data_dict
+print(f'apakah {KEY} ada di data_dict: {CHECKKEY}') # Jika key yang kita ingin periksa ada di dalam data_dict, maka hasilnya adalah True, jika sebaliknya maka hasilnya adalah False
+
+# mengakses value (read) atau nilai di dalam dictionary menggunakan get
+print(data_dict["cup"])
+print(data_dict.get("cup")) # untuk memastikan bahwa data yang ingin diakses adalah dictionary dan bukan data yang lain
+print(data_dict.get("kis")) # saat menggunakan get, jika kita mengakses data yang tidak ada di dalam dictionary tersebut, maka tidak akan error, melainkan hanya menampilkan pesan 'none' saja.
+print(data_dict.get("kis", "key tidak ditemukan")) # atau kita juga bisa menambahkan pesan tambahan jika key yang ingin diakses tidak tersedia
+
+# mengupdate data
+# mengubah nilai dari data yang sudah ada
+data_dict["cup"] = "ucup si ganteng"
+print(data_dict)
+
+# menambahkan data baru yang belum ada
+data_dict["sep"] = "asep si kasyep"
+print(data_dict)
+
+# mengembalikan data ke semula jika datanya pernah ada
+data_dict.update({"cup": "ucup surucup"})
+print(data_dict)
+
+data_dict.update({"rizky": "rizky si programmer handal"}) # jika memakai fungsi .update untuk data yang belum ada, maka akan otomatis menambahkan data yang baru ini ke dalam data yang sudah ada
+print(data_dict)
+
+# delete data pada dictionary
+del data_dict["rizky"] # Jika ingin menghapus data yang ada di dalam dictionary, maka kita bisa gunakan kode del dan pada kurung perseginya kita masukan key dari data yang ingin kita hapus
+print(data_dict)
