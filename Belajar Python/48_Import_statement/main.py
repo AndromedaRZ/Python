cdbbe554e62861990641526fef7791d513cbf817
Rizky <rizkyricky859@gmail.com>
2025-09-24 14:29:39 +0700

    update file belajar

--- Belajar Python/48_Import_statement/main.py
+++ Belajar Python/48_Import_statement/main.py
@@ -1,11 +1,17 @@
 # import statement
 
-# salah satu fungsi import adalah untuk mengambil program dari file eksternal (.py)
+'''
+import pada python digunakan untuk memanggil (menggunakan) kode dari luar file python kita, bisa berupa:
+- Modul bawaan python (library standar seperti math, random, oss, dll)
+- Modul buatan sendiri atau file python eksternal yang kita buat
+- Modul dari pihak ketiga (library yang diinstal dengan pip seperti numpy, pandas, dll)
+
+Dengan menggunakan import, kita tidak perlu lagi menulis ulang sebuah kode, tinggal ambil saja kode yang sudah pernah kita buat'''
 
 # 1. untuk menjalankan program dari file eksternal
 import program_print
 
-# 2. import dengan data
+# 2. import data dari file eksternal
 import variabel
 
 # data ada di namescape variabel
@@ -13,7 +19,7 @@ print(variabel.data) # cara mengambil data dari file lain menggunakan import ada
 
 # variabel = namescapenya
 
-# 3. import dengan fungsi
+# 3. import fungsi dari file eksternal
 import matematika
 
 hasil = matematika.tambah(1, 3) # cara mengambil fungsi dari file lain menggunakan import adalah dengan mengetikan nama filenya (matematika) lalu ketikan nama fungsi yang ada di dalam file tersebut di sebelah kanan filenya dan pisahkan dengan titik
