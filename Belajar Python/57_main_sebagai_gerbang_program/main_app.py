e2952db53198d434c5c1631981b09da4afef3fe8
Rizky <rizkyricky859@gmail.com>
2025-10-07 12:58:10 +0700

    update file belajar

---
+++ Belajar Python/57_main_sebagai_gerbang_program/main_app.py
@@ -0,0 +1,24 @@
+# __main__ adalah top level environment
+
+# __name__ = "__main__" akan terjadi jika ada di file program utama
+
+## __name__ pada file program utama
+print(f'nilai __name__ pada main.py = {__name__}')
+
+## __name__ pada file program eksternal
+import fungsi
+
+# deklarasi
+def fungsi_kali(a:int, b:int)->int:
+    '''fungsi kali'''
+    return a * b
+
+# fungsi utama
+if __name__ == '__main__':
+    angka1 = 5
+    angka2 = 10
+    hasil = fungsi_kali(angka1, angka2)
+    print(f'hasil {angka1} kali {angka2} = {hasil}')
+    
+## import pacakge
+import package
